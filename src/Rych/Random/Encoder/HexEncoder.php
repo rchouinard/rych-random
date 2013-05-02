00ac8861c8edf92b8da54931739fc31ecd9db846
Ryan Chouinard <rchouinard@gmail.com>
2013-05-02 14:07:10 -0400

    Add hex encoder.
    Also remove old generator test.

---
+++ src/Rych/Random/Encoder/HexEncoder.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Ryan's Random Data Library
+ *
+ * @package Rych\Random
+ * @author Ryan Chouinard <rchouinard@gmail.com>
+ * @copyright Copyright (c) 2013, Ryan Chouinard
+ * @license MIT License - http://www.opensource.org/licenses/mit-license.php
+ */
+
+namespace Rych\Random\Encoder;
+
+/**
+ * Hex encoder
+ *
+ * @package Rych\Random
+ * @author Ryan Chouinard <rchouinard@gmail.com>
+ * @copyright Copyright (c) 2013, Ryan Chouinard
+ * @license MIT License - http://www.opensource.org/licenses/mit-license.php
+ */
+class HexEncoder implements EncoderInterface
+{
+
+    /**
+     * Encode a string of raw bytes.
+     *
+     * @param string $string
+     * @return string
+     */
+    public function encode($string)
+    {
+        return bin2hex($string);
+    }
+
+    /**
+     * Decode an encoded string.
+     *
+     * @param string $string
+     * @return string
+     */
+    public function decode($string)
+    {
+        // hex2bin was not introduced until PHP 5.4
+        return pack('H*' , $string);
+    }
+
+}

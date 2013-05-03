43749d9f60d8828545739ef0e95aead2c1ef1eeb
Ryan Chouinard <rchouinard@gmail.com>
2013-05-03 14:07:23 -0400

    Use RFC test vectors in Base64 encoder test.

--- tests/unit/Rych/Random/Encoder/Base64EncoderTest.php
+++ tests/unit/Rych/Random/Encoder/Base64EncoderTest.php
@@ -10,21 +10,29 @@ class Base64EncoderTest extends TestCase
     public function testEncodeMethodPrducesExpectedResult()
     {
         $encoder = new Base64Encoder;
+
+        // RFC 4648 test vectors
         $this->assertEquals('', $encoder->encode(''));
-        $this->assertEquals('QQ==', $encoder->encode('A'));
-        $this->assertEquals('QUI=', $encoder->encode('AB'));
-        $this->assertEquals('QUJD', $encoder->encode('ABC'));
-        $this->assertEquals('QUJDRA==', $encoder->encode('ABCD'));
+        $this->assertEquals('Zg==', $encoder->encode('f'));
+        $this->assertEquals('Zm8=', $encoder->encode('fo'));
+        $this->assertEquals('Zm9v', $encoder->encode('foo'));
+        $this->assertEquals('Zm9vYg==', $encoder->encode('foob'));
+        $this->assertEquals('Zm9vYmE=', $encoder->encode('fooba'));
+        $this->assertEquals('Zm9vYmFy', $encoder->encode('foobar'));
     }
 
     public function testDecodeMethodProducesExpectedResult()
     {
         $encoder = new Base64Encoder;
+
+        // RFC 4648 test vectors
         $this->assertEquals('', $encoder->decode(''));
-        $this->assertEquals('A', $encoder->decode('QQ=='));
-        $this->assertEquals('AB', $encoder->decode('QUI='));
-        $this->assertEquals('ABC', $encoder->decode('QUJD'));
-        $this->assertEquals('ABCD', $encoder->decode('QUJDRA=='));
+        $this->assertEquals('f', $encoder->decode('Zg=='));
+        $this->assertEquals('fo', $encoder->decode('Zm8='));
+        $this->assertEquals('foo', $encoder->decode('Zm9v'));
+        $this->assertEquals('foob', $encoder->decode('Zm9vYg=='));
+        $this->assertEquals('fooba', $encoder->decode('Zm9vYmE='));
+        $this->assertEquals('foobar', $encoder->decode('Zm9vYmFy'));
     }
 
 }
